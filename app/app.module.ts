e1b4d28eaedf2a50e1b3bce8ffc6a69727b2329d
stephengtuggy <sgt@stephengtuggy.com>
2016-12-27 14:16:16 -0800

    Tour of Heroes Part 5(A)

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,42 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
+import { RouterModule }         from '@angular/router';
 
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { AppComponent }         from './app.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroService }          from './hero.service';
 
 @NgModule({
   imports:      [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dasbhoard',
+        pathMatch: 'full'
+      }
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  providers:    [
+    HeroService
   ],
   bootstrap:    [ AppComponent ]
 })
