ff63415d0fb79ecca1cc4f01bc18758723f5518c
stephengtuggy <sgt@stephengtuggy.com>
2017-05-18 14:07:32 -0700

    app.e2e-spec.ts: Try a different way

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,29 @@
-import { browser, element, by } from 'protractor';
+import { browser, element, by, promise } from 'protractor';
 
 describe('QuickStart E2E Tests', function () {
 
-  let expectedMsg = 'Tour of Heroes';
+  let expectedMsg = 'Tour of Heroez';
+  let deferred = promise.defer();
 
   beforeEach(function () {
     browser.get('');
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
-  });
+  function headerMatchesExpected(element: any) {
+    element.getText().then(
+      function success(text: any) {
+        if (text === expectedMsg) {
+          deferred.fulfill(text);
+        } else {
+          deferred.reject('H1 element did not contain the expected text');
+        }
+      },
+      function error(reason: any) {
+        deferred.reject(reason);
+      }
+    );
+  }
+
+  let headingElement = element(by.css('h1'));
 
 });
